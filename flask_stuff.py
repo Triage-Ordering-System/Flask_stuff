711c15a614b888b5b38da0c302e56fd2b05b3962
dsarca02 <126446353+dsarca02@users.noreply.github.com>
2023-11-05 02:41:54 -0500

    placeholder Patient Class flask_stuff.py
    used placeholders for the Patient class

--- flask_stuff.py
+++ flask_stuff.py
@@ -1,38 +1,50 @@
-# flask_stuff.py
-
 from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
-
-# Import the placeholder functions from processing.py
-import processing as p
+from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins="http://localhost:3000")
+CORS(app)
 
-@app.route("/process_data", methods=["POST"])
-@cross_origin(supports_credentials=True)
-def process_data():
+# This is a placeholder for the Patient class.
+# You would replace this with the actual class code that you provide later.
+class Patient:
+    def __init__(self, name, age, symptoms):
+        self.name = name
+        self.age = age
+        self.symptoms = symptoms
+        # You can add more attributes here based on the actual Patient class definition.
+
+patients = []
+
+@app.route('/receive_data', methods=['POST'])
+def receive_data():
+    data = request.json
     try:
-        # Get the data from the JSON sent by the frontend
-        data = request.json
-        name = data.get("name")
-        age = data.get("age")
-
-        # Use imported functions to process the data
-        processed_name = p.process_name(name)
-        processed_age = p.process_age(age)
-
-        # Prepare the result with the processed data
-        result = {
-            "processed_name": processed_name,
-            "processed_age": processed_age,
-        }
-
-        # Return the result as JSON
-        return jsonify(result)
-
+        # Create a Patient object with the incoming data
+        patient = Patient(
+            name=data.get('name'),
+            age=data.get('age'),
+            symptoms={
+                'name': data.get('symptom_name'),
+                'severity': data.get('severity'),
+                'time': data.get('time')
+            }
+        )
+        # Store the patient object in a list (you might want to use a database in production)
+        patients.append(patient)
+        # Acknowledge the receipt of patient data
+        return jsonify({'message': 'Patient data received successfully.'}), 200
     except Exception as e:
-        return jsonify({"error": str(e)})
+        return jsonify({'error': str(e)}), 500
 
-if __name__ == "__main__":
+@app.route('/patients', methods=['GET'])
+def get_patients():
+    try:
+        # Assuming there is a method to serialize Patient objects to a dictionary
+        # This is just a placeholder to show the structure
+        patients_data = [{'name': patient.name, 'age': patient.age, 'symptoms': patient.symptoms} for patient in patients]
+        return jsonify(patients_data), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
     app.run(debug=True)
