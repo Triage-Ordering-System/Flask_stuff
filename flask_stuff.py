1a1acf641d7c6d1a8c627be985d9c1297691d5b7
dsarca02 <126446353+dsarca02@users.noreply.github.com>
2023-11-05 01:12:05 -0500

    Update flask_stuff.py
    p.process_name = placeholders for functions

--- flask_stuff.py
+++ flask_stuff.py
@@ -1,27 +1,38 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-from flask_cors import cross_origin
-import sys
+# flask_stuff.py
 
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+
+# Import the placeholder functions from processing.py
+import processing as p
 
 app = Flask(__name__)
 CORS(app, origins="http://localhost:3000")
 
-
-# Create a route to handle POST requests for processing data
 @app.route("/process_data", methods=["POST"])
 @cross_origin(supports_credentials=True)
 def process_data():
     try:
-        # Get the 'code' value from the JSON data sent by the frontend
+        # Get the data from the JSON sent by the frontend
         data = request.json
         name = data.get("name")
-        age = data.get("name")
-        # Check for additional keys to determine the action
+        age = data.get("age")
 
-        # Call the main function and return its result
-        result = main(code)
+        # Use imported functions to process the data
+        processed_name = p.process_name(name)
+        processed_age = p.process_age(age)
+
+        # Prepare the result with the processed data
+        result = {
+            "processed_name": processed_name,
+            "processed_age": processed_age,
+        }
+
+        # Return the result as JSON
         return jsonify(result)
 
     except Exception as e:
         return jsonify({"error": str(e)})
+
+if __name__ == "__main__":
+    app.run(debug=True)
